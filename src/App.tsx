332a4e001b4a203c1c2a68a03831d800bc5c2047
ZhenyaGro <zhenyagro@icloud.com>
2025-01-23 16:54:40 +0300

    Themes, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { Link } from 'react-router-dom';
 import { MainPageAsync } from './pages/MainPage/MainPageAsync';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div onClick={toggleTheme} className={`app ${theme}`}>
+      <button>Toggle theme</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/about' element={<AboutPageAsync />} />
-          <Route path='/' element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
         </Routes>
       </Suspense>
-
     </div>
-  )
-}
+  );
+};
