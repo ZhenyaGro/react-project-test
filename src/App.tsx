1f3a804600651f9cea093fdba8764a74cee49160
ZhenyaGro <zhenyagro@icloud.com>
2025-01-23 14:54:08 +0300

    Router, Lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import Counter from './components/Counter';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { Link } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPageAsync';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { Suspense } from 'react';
 
 export const App = () => {
   return (
     <div className='app'>
-      Test
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/about' element={<AboutPageAsync />} />
+          <Route path='/' element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+
     </div>
   )
 }
