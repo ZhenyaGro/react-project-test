62bb0308c3ff27e796678d905cc2b3be315e1bee
ZhenyaGro <zhenyagro@icloud.com>
2025-01-24 16:32:54 +0300

    Folder structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import { Link } from 'react-router-dom';
-import { MainPageAsync } from './pages/MainPage/MainPageAsync';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames';
+import { useTheme } from './providers/ThemeProvider/lib/useTheme';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { AboutPage } from '@/pages/AboutPage';
+import { MainPage } from '@/pages/MainPage';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,8 +17,8 @@ export const App = () => {
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
