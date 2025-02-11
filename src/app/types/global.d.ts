5ec768b08027700669e90604016eef3acef41f38
ZhenyaGro <zhenyagro@icloud.com>
2025-02-11 15:31:24 +0300

    Svg, file loaders

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,7 +1,16 @@
-declare module "*.scss" {
+declare module '*.scss' {
   interface IClassNames {
     [className: string]: string;
   }
   const classNames: IClassNames;
   export = classNames;
 }
+
+declare module '*.svg' {
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
